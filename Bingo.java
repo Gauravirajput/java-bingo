978b637e614768be576f6c20539abde99aa3b6f1
Yoon Wai Yan <yoonwaiyan92@gmail.com>
2013-12-20 21:16:42 +0800

    Remove accidential java file

--- Bingo.java
+++
@@ -1,8 +0,0 @@
-package bingo;
-
-public class Bingo{
-	public void printHello()
-	{
-		System.out.println("Hello World");
-	}
-}
--- BingoServer.java
+++
@@ -1,9 +0,0 @@
-package bingo;
-
-public class BingoServer{
-	public static void main(String[] args)
-	{
-		Bingo b = new Bingo();
-		b.printHello();
-	}
-}
