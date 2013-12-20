aafed7b54cf9b5a5b4826c282e844b12562b214b
Yoon Wai Yan <yoonwaiyan92@gmail.com>
2013-12-21 00:18:03 +0800

    Allow client to select number of cards

--- src/BingoView.java
+++ src/BingoView.java
@@ -1,18 +1,34 @@
 package bingo;
 
+import java.awt.*;
 import javax.swing.*;
 
 // View class
 public class BingoView{
+
 	public void displayMainPage()
 	{
 		//Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
+        JFrame frame = new JFrame("Bingo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
-        frame.getContentPane().add(label);
+ 		GridLayout mainPage = new GridLayout(2,2);
+        JPanel mainPagePanel = new JPanel();
+        mainPagePanel.setLayout(mainPage);
+
+        // Initialize grid layout and several buttons for number of cards
+		JButton oneCard = new JButton("1 Card");
+		JButton twoCards = new JButton("2 Cards");
+		JButton threeCards = new JButton("3 Cards");
+		JButton fourCards = new JButton("4 Cards");
+
+        mainPagePanel.add(oneCard);
+        mainPagePanel.add(twoCards);
+        mainPagePanel.add(threeCards);
+        mainPagePanel.add(fourCards);
+
+        // Add mainPage to the frame
+        frame.getContentPane().add(mainPagePanel);
  
         //Display the window.
         frame.pack();
