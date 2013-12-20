9bae7876aea41ba91b15c28c08e37a4f9f2857d5
Yoon Wai Yan <yoonwaiyan92@gmail.com>
2013-12-20 22:03:37 +0800

    Initial socket settings for client and server

--- src/BingoServer.java
+++ src/BingoServer.java
@@ -1,9 +1,42 @@
-package bingo;
+import bingo.Bingo;
+import java.io.*;
+import java.net.*;
 
 public class BingoServer{
 	public static void main(String[] args)
 	{
-		Bingo b = new Bingo();
-		b.printHello();
+		if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port number>");
+            System.exit(1);
+        }
+ 
+        int portNumber = Integer.parseInt(args[0]);
+ 
+        try (
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+         
+            String inputLine, outputLine;
+             
+            // Initiate conversation with client
+           	outputLine = " ";
+            out.println(outputLine);
+ 
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = inputLine + " from server";
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
 	}
 }
